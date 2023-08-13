d11d86410f47d8781de82319b7205e320c02d63b
Chris Walsh <chris.is.rad@pm.me>
2023-08-13 04:56:28 -0400

    added some more documentation

--- geoquery/geode.py
+++ geoquery/geode.py
@@ -1,9 +1,19 @@
 import json
+import math
 from urllib import request
 from urllib.parse import urlunsplit, urlencode
 
 
 def get_lat_long(street_address: str) -> tuple:
+    """gets a latitude/longitude pair from street address
+
+    Parameters:
+        street_address (str): address query
+
+    Returns:
+        tuple: a coordinate.
+
+    """
     qry = urlencode(dict(
         format="json", addressdetails=1, polygon=1,
         q=street_address.strip()
@@ -66,3 +76,41 @@ def get_observation(station_id):
         data = json.loads(res.read())
 
         return data['features']
+
+
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371.0  # earth's radius in km.
+
+    # convert degrees to radians
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    return R * c  # distance
+
+
+def get_nearby_coordinates(center_lat, center_lon, radius):
+    """gets nearby coordinates within a radius in kilometers
+
+    Parameters:
+        center_lat (double): center latitude
+        center_lon (double): center longitude
+        radius (int): radius in kilometers
+
+    Returns:
+        list: a list of coordinate tuples (lat, lon)
+
+    """
+
+    nearby_coords = []
+
+    for lat in range(-90, 91, 1):
+        for lon in range(-180, 181, 1):
+            distance = haversine(center_lat, center_lon, lat, lon)
+
+            if distance <= radius:
+                nearby_coords.append((lat, lon))
+
+    return nearby_coords
