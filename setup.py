e834fe47a66948b983125e67f3720417a52c26d5
Chris Walsh <chris.is.rad@pm.me>
2025-01-03 18:33:47 -0500

    release 0.0.1

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 from setuptools import setup, find_packages
 
+reqs = []
+
 setup(
     name="geoquery",
     author="Chris Walsh",
     author_email="chris.is.rad@pm.me",
-    classifiers=[],
     description="a geo querier",
     license="MIT",
     version="0.0.1",
-    url="",
     packages=find_packages(),
-    install_rquires=[],
+    install_rquires=reqs,
     entry_points={
         'console_scripts': [
             'gregory = geoquery.main:boot_up'
         ]
-    }
+    },
+    python_requires='>=3.12',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent'
+    ]
 )
